17cc8236facd50af0e3be071fec938de70f9368e
rafif <irfan.rafif@geniebook.com>
2023-01-14 21:06:52 +0700

    setup firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tyrasoft_attendance/pages/web_select.dart';
 
 import 'bloc/url_bloc.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
   runApp(const MyApp());
 }
 
